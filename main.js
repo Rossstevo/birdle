73f26457b2b904a76c8932ba4d3e64621a9e8672
Ross Stevenson <Ross.w.Stevenson@gmail.com>
2022-04-07 12:04:45 +0100

    changed the picture to Bowie

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ let answer = "david bowie"
 let toggle = true;
 
 //watsap share feature 
-const linkStart = "whatsapp://send?text=I got todays Peeple in "
-const linkEnd = "https://sparkly-mandazi-c28bd8.netlify.app/"
-let linkText = linkStart + count + linkEnd;
-console.log(linkText);
-const link = document.getElementById("link");
-document.getElementById("link").innerHTML = link
+// const linkStart = "whatsapp://send?text=I got todays Peeple in "
+// const linkEnd = "https://sparkly-mandazi-c28bd8.netlify.app/"
+// let linkText = linkStart + count + linkEnd;
+// console.log(linkText);
+// const link = document.getElementById("link");
+// document.getElementById("link").innerHTML = link
 
 
 console.log("toggle count = " + toggle)
